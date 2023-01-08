7bd79efc305003fe9fe3178f0f468e55534751a0
IvorTech <eldinamitmc@gmail.com>
2023-01-08 16:55:56 -0300

    Update countdown.js

--- assets/js/vendors/countdown.js
+++ assets/js/vendors/countdown.js
@@ -1,1 +1,2 @@
 !function(){var t,e;$(".countdown").downCount({date:((t=new Date).setDate(t.getDate()+10),e=t.getDate(),t.getMonth()+1+"/"+e+"/"+t.getFullYear()+" 12:00:00"),offset:0})}();
+console.log(t.getDate()+10);
